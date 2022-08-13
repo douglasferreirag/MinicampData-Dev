8462d508ea12cb06b865321c7b627942b7b32f46
Douglas <douglas.ferreirag@gmail.com>
2022-08-13 20:58:11 -0300

    IMC parte 3

--- 06 - IMC/js/script.js
+++ 06 - IMC/js/script.js
@@ -1,11 +1,27 @@
 function start() {
   var buttonCalculateImc = document.querySelector('#button-calculate-imc');
-
   buttonCalculateImc.addEventListener('click', handleButtonClick);
+
+  var inputWeight = document.querySelector('#input-weight');
+  var inputHeight = document.querySelector('#input-height');
+
+  inputWeight.addEventListener('click', handleButtonClick);
+  inputHeight.addEventListener('click', handleButtonClick);
+
+  handleButtonClick();
 }
 
 function handleButtonClick() {
-  console.log('cliquei');
+  var inputWeight = document.querySelector('#input-weight');
+  var inputHeight = document.querySelector('#input-height');
+  var imcResult = document.querySelector('#imc-result');
+
+  var weight = Number(inputWeight.value);
+  var height = Number(inputHeight.value);
+
+  var imc = calculateImc(weight, height);
+  var formattedImc = imc.toFixed(2).replace('.', ',');
+  imcResult.textContent = formattedImc;
 }
 
 function calculateImc(weight, height) {
