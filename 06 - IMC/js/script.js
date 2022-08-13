1a2e014e2f5b88016dce6b84251b58f15fc9a675
Douglas <douglas.ferreirag@gmail.com>
2022-08-13 19:37:50 -0300

    IMC parte 2

--- 06 - IMC/js/script.js
+++ 06 - IMC/js/script.js
@@ -1,1 +1,15 @@
-console.log('Teste no console do navegador.');
+function start() {
+  var buttonCalculateImc = document.querySelector('#button-calculate-imc');
+
+  buttonCalculateImc.addEventListener('click', handleButtonClick);
+}
+
+function handleButtonClick() {
+  console.log('cliquei');
+}
+
+function calculateImc(weight, height) {
+  return weight / (height * height);
+}
+
+start();
