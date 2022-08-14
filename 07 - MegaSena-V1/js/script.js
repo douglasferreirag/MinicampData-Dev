f30c6c32db56c9a827213c75ec4fb0c57f11fd06
Douglas <douglas.ferreirag@gmail.com>
2022-08-14 20:12:14 -0300

    Megasena-v1-parte2

--- 07 - MegaSena-V1/js/script.js
+++ 07 - MegaSena-V1/js/script.js
@@ -6,17 +6,12 @@ function start() {
   addNumberToGame(3);
   addNumberToGame(4);
   addNumberToGame(5);
-  addNumberToGame(60);
-  addNumberToGame(59);
-  addNumberToGame(59);
-  addNumberToGame(59);
-  addNumberToGame(59);
-  addNumberToGame(59);
-  addNumberToGame(59);
-  addNumberToGame(59);
-  addNumberToGame(59);
-  addNumberToGame(62);
+  saveGame();
+  addNumberToGame(6);
+  saveGame();
+  saveGame();
   console.log(state.currentGame);
+  console.log(state.savedGames);
 }
 
 function addNumberToGame(numberToAdd) {
@@ -24,12 +19,59 @@ function addNumberToGame(numberToAdd) {
     console.error('Número inválido', numberToAdd);
     return;
   }
-  if (state.currentGame.length) {
+  if (state.currentGame.length >= 6) {
     console.error('O jogo está completo.');
     return;
   }
 
+  if (isNumberInGame(numberToAdd)) {
+    console.error('Este número já está no jogo', numberToAdd);
+    return;
+  }
+
   state.currentGame.push(numberToAdd);
+}
+
+function removeNumberFromGame(numberToRemove) {
+  if (numberToRemove < 1 || numberToRemove > 60) {
+    console.error('Número inválido', numberToRemove);
+    return;
+  }
+
+  var newGame = [];
+
+  for (var i = 0; i < state.currentGame.length; i++) {
+    var currentNumber = state.currentGame[i];
+
+    if (currentNumber === numberToRemove) {
+      continue;
+    }
+
+    newGame.push(currentNumber);
+  }
+
+  state.currentGame = newGame;
+}
+
+function saveGame() {
+  if (!isGameComplete()) {
+    console.error('O jogo não está completo!');
+    return;
+  }
+
+  state.savedGames.push(state.currentGame);
+}
+
+function isGameComplete() {
+  return state.currentGame.length === 6;
+}
+
+function isNumberInGame(numberToCheck) {
+  /*if (states.currentGame.includes(numberToCheck)) {
+    return true;
+  }*/
+
+  return state.currentGame.includes(numberToCheck);
 }
 
 start();
